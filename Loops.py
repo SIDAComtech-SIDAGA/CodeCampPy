0833086bdd3d027724ef0c86f47a18e1bf15f3a3
sidaga kihongo <sidagawazirikihongo@gmail.com>
2024-05-30 13:06:38 +0300

    Sidaga Waziri

--- Loops.py
+++ Loops.py
@@ -8,3 +8,19 @@
 # for num in range(8):
 #     print("Hello" * num)
 
+# my_list = ["a","b","c","d","e"]
+# for i in range(len(my_list)):
+#     print(my_list[i])
+    
+    
+    
+# for i in range(2,10):
+#     print(i)
+
+# list = ["a","b","c","d","e"]
+# for i in range(2, len(list)):
+#     print(list[i])
+
+    
+# for j in range(10,5,-1):
+#     print(j)
