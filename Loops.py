ecdfe5db2afd81df622a7cc056e61431a0a47e39
sidaga kihongo <sidagawazirikihongo@gmail.com>
2024-05-30 13:16:47 +0300

    HHH

--- Loops.py
+++ Loops.py
@@ -24,3 +24,22 @@
     
 # for j in range(10,5,-1):
 #     print(j)
+
+# message = "Hello, World!"
+# for i in message:
+#     print(i)
+
+# word = "Hello"
+# for i in word.upper():
+#     print(i)
+
+# Mom = (2,3,4,5,6,7,8,9,10)
+# for i in Mom:
+#     if i%2 == 0:
+#         print("Even")
+#     else:
+#         print("Odd")
+
+my_dic = {"a":1, "b":2, "c":3}
+for i in my_dic.values():
+    print(i)
